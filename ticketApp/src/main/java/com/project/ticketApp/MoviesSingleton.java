62ca66efa4a40a2bd731318e47660076f5710e35
TrumanYen <yen.truman@gmail.com>
2022-11-28 20:22:46 -0700

    ticket controller

---
+++ ticketApp/src/main/java/com/project/ticketApp/MoviesSingleton.java
@@ -0,0 +1,17 @@
+package com.project.ticketApp;
+
+public class MoviesSingleton extends Singleton<Movie>{
+
+    @Override
+    public void addObj(Movie obj) {
+        // TODO Auto-generated method stub
+        super.addObj(obj);
+    }
+
+    @Override
+    public Movie getObj(int objID) {
+        // TODO Auto-generated method stub
+        return super.getObj(objID);
+    }
+    
+}
--- ticketApp/src/main/java/com/project/ticketApp/TicketController.java
+++ ticketApp/src/main/java/com/project/ticketApp/TicketController.java
@@ -1,6 +1,18 @@
 package com.project.ticketApp;
 
 public class TicketController {
+    private MainController parentController;
     private TicketGUI gui;
 
+    public TicketController(MainController parentController){
+
+    }
+    public void viewTicket(int ticketID){
+
+    }
+    public void refundTicket(Ticket ticket){
+
+    }
+    private void makeGUI(Ticket ticket){}
+    
 }
