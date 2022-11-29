51ff8b959e0a73d7f7770e3ca521811fd0f01723
666dragonz <93061675+tanishdatta@users.noreply.github.com>
2022-11-28 19:41:52 -0700

    Update OfferedMovies.java

--- ticketApp/src/main/java/com/project/ticketApp/OfferedMovies.java
+++ ticketApp/src/main/java/com/project/ticketApp/OfferedMovies.java
@@ -1,5 +1,14 @@
 package com.project.ticketApp;
 
+import java.util.ArrayList;
+
 public class OfferedMovies {
+private Movie movie; 
+private ArrayList<Showtime> showtimes;
+
+public ArrayList <Showtime> getShowtimes(){
+    return showtimes; 
+}
+
 
 }
