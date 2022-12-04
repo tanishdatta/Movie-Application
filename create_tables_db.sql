180713d7186dc2daf2065dfebba435152d6b9c0c
666dragonz <93061675+tanishdatta@users.noreply.github.com>
2022-12-04 02:13:34 -0700

    added query for db

---
+++ create_tables_db.sql
@@ -0,0 +1,102 @@
+USE ticketreservation_db;
+
+CREATE TABLE RegisteredUser (
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    name VARCHAR(50),
+    credit_card_num INT NOT NULL,
+    credit_card_holder VARCHAR(50) NOT NULL,
+    last_payment DATE NOT NULL,
+    PRIMARY KEY (username)
+);
+
+CREATE TABLE Payment (
+    payment_id INT NOT NULL,
+    credit_card_num INT NOT NULL,
+    credit_card_holder VARCHAR(50) NOT NULL,
+    local_date DATE NOT NULL,
+    PRIMARY KEY (payment_id)
+);
+
+CREATE TABLE theatre (
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(70) NOT NULL,
+    PRIMARY KEY (name),
+    UNIQUE (location)
+);
+
+CREATE TABLE movie (
+    name VARCHAR(50) NOT NULL,
+    announcement_date DATE NOT NULL,
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE offered_movie (
+    movie_name VARCHAR(50) NOT NULL,
+    theatre_name VARCHAR(50) NOT NULL,
+    FOREIGN KEY (movie_name)
+        REFERENCES movie (name)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (theatre_name)
+        REFERENCES theatre (name)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+	
+
+CREATE TABLE showtime (
+    movie_name VARCHAR(50) NOT NULL,
+    theatre_name VARCHAR(50) NOT NULL,
+    date_time DATETIME NOT NULL,
+    CONSTRAINT showtime_pk PRIMARY KEY (movie_name , theatre_name , date_time),
+    FOREIGN KEY (movie_name)
+        REFERENCES movie (name)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (theatre_name)
+        REFERENCES theatre (name)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+CREATE TABLE ticket (
+    ticket_id INT NOT NULL,
+    showtime_time DATETIME NOT NULL,
+    theatre_name VARCHAR(50) NOT NULL,
+    movie_name VARCHAR(50) NOT NULL,
+    seatXcoord INT NOT NULL,
+    seatYcoord INT NOT NULL,
+    price INT NOT NULL,
+    PRIMARY KEY (ticket_id),
+    FOREIGN KEY (theatre_name)
+        REFERENCES theatre (name)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (movie_name)
+        REFERENCES movie (name)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE refund_credit (
+    credit_code INT NOT NULL,
+    dollar_amount INT NOT NULL,
+    PRIMARY KEY (credit_code)
+);
+
+CREATE TABLE seat (
+    seatXcoord INT NOT NULL,
+    seatYcoord INT NOT NULL,
+    theatre_name VARCHAR(50) NOT NULL,
+    movie_name VARCHAR(50) NOT NULL,
+    showtime_time DATETIME NOT NULL,
+    status BIT,
+    CONSTRAINT seat_pk PRIMARY KEY (seatXcoord , seatYcoord),
+    FOREIGN KEY (theatre_name)
+        REFERENCES theatre (name)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (movie_name)
+        REFERENCES movie (name)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+
+
+
+
---
+++ populate_db.sql
@@ -0,0 +1,34 @@
+USE ticketreservation_db;
+
+
+INSERT INTO registereduser (username, password, name, credit_card_num, credit_card_holder, last_payment) VALUES
+('Tanish1', 'ensf', 'Tanish', 1234, 'Tanish', '2022-12-4'),
+('Truman2', 'ensf', 'Trunam', 1234, 'Trunam', '2022-12-4'),
+('Max3', 'ensf', 'Max', 1234, 'Max', '2022-12-4'),
+('Shanelle4', 'ensf', 'Shanelle', 1234, 'Shanelle', '2022-12-4'),
+('Harnoor5', 'ensf', 'Harnoor', 1234, 'Harnoor', '2022-12-4');
+
+INSERT INTO movie (name, announcement_date) VALUES
+('Star Wars: Episode VII - The Force Awakens', '2015-01-01'),
+('Avengers: Endgame', '2019-01-01'),
+('Spider-Man: No Way Home', '2021-01-01'),
+('Avatar','2009-01-01'),
+('Top Gun: Maverick', '2022-01-01');
+
+INSERT INTO theatre (name, location) VALUES
+('WISH.com_cineplex', 'somewhere');
+
+INSERT INTO showtime (movie_name, theatre_name, date_time) VALUES
+('Star Wars: Episode VII - The Force Awakens','WISH.com_cineplex', '17:10:10'),
+('Avengers: Endgame','WISH.com_cineplex', '13:10:10'),
+('Spider-Man: No Way Home','WISH.com_cineplex', '10:00:00'),
+('Avatar','WISH.com_cineplex', '06:00:00'),
+('Top Gun: Maverick','WISH.com_cineplex', '20:00:00');
+
+INSERT INTO offered_movie (movie_name, theatre_name) VALUES
+('Star Wars: Episode VII - The Force Awakens','WISH.com_cineplex'),
+('Avengers: Endgame','WISH.com_cineplex'),
+('Spider-Man: No Way Home','WISH.com_cineplex');
+
+
+
