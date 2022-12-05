8d592b58e842a1bb2be624cfbd190cf0bb406bbb
max584 <maxcout584@gmail.com>
2022-12-04 17:53:11 -0700

    MovieFilterStrategy

--- ticketApp/src/main/java/com/project/ticketApp/TwoDaysPrior.java
+++ ticketApp/src/main/java/com/project/ticketApp/TwoDaysPrior.java
@@ -1,13 +1,19 @@
 package com.project.ticketApp;
+import java.time.LocalDate;
 import java.util.ArrayList;
 //Stub Finished
 public class TwoDaysPrior implements MovieFilterStrategy{
 
 	@Override
 	public ArrayList<OfferedMovie> filterMovies(ArrayList<OfferedMovie> inputList) {
+		for(OfferedMovie i : inputList){
+			if(i.getMovie().getAnnouncementDate().isAfter(LocalDate.now())){
+				inputList.remove(i);
+			}
+		}
 		//If announcementDate is later than two days from now take it out
 		//return the list
-		return null;
+		return inputList;
 	}
 
 }
